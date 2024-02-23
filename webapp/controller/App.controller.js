b996fae7b21dbf8cf691f79acb22f4af15029b21
AnaIranzo <aeiranzom@gmail.com>
2024-02-23 14:07:37 +0100

    steps 11 to 15

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,14 +6,5 @@ sap.ui.define([
     "use strict"
     return Controller.extend("ui5.walkthrough.controller.App",{
     
-        onShowHello : function () {
-        // read msg from i18n model
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-         // show message
-            MessageToast.show(sMsg);   
-        }
     });
 })
